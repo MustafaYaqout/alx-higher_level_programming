9af5802fdd83effc0df67f5db7b94c0612371ae3
MustafaYaqout <mustafa.yaqoutt@gmail.com>
2023-12-03 22:25:31 -0800

    push

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list[i] % 2 = 0:
-        print("{:d} is divisible by 2".format(my_list[i]) + ture)
-    else:
-        print("{:d} is divisible by 2".format(my_list[i]) + false)
-
+    for i in my_list:
+        if my_list[i] % 2 == 0:
+            print("{:d} is divisible by 2".format(my_list[i]) + ture)
+        else:
+            print("{:d} is divisible by 2".format(my_list[i]) + false)
