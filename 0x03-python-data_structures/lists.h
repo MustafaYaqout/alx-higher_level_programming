e5157c6d44123e2549e4d3a1f7a5a9b9adf48934
MustafaYaqout <mustafa.yaqoutt@gmail.com>
2023-12-04 05:02:13 -0800

    push agian

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,4 +1,14 @@
+#ifndef LISTS_H
+#define LISTS_H
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for project
+ */
 typedef struct listint_s
 {
     int n;
