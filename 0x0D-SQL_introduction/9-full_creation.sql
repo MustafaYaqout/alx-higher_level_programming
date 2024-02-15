847f6b9df425429954b782ba050b7ac116848e96
MustafaYaqout <mustafa.yaqoutt@gmail.com>
2024-02-15 06:05:26 -0800

    all

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---create a new database hbtn_0c_0 in your MySQL server
+-- create a new database hbtn_0c_0 in your MySQL server
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
