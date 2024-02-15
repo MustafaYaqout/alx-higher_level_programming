67b01aa700aef1ae0c2e336d10935a5d2455499f
MustafaYaqout <mustafa.yaqoutt@gmail.com>
2024-02-15 05:49:31 -0800

    push

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- Create a table called first_table with two columns id and name. id should be an integer and name should be a string.
-CREATE TABLE IF NOT EXISTS first_table (
-	    id INT,
-	    name VARCHAR(256)
-	);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
